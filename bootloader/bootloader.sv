2c633d1150b7de48f03c53798112c1821c0e692b
Aidan Mulvaney <amulvaney@wisc.edu>
2024-11-15 14:48:23 -0600

    add functionality to send out timing info of how long calculation took

--- bootloader/bootloader.sv
+++ bootloader/bootloader.sv
@@ -13,6 +13,7 @@ module bootloader(
     input wire done_drawing,              // signifies when the hardware has finished drawing to the frame buffer
     input wire bootload_en, transmit_en,  // these signals should be mapped to switches on the board
     input wire [23:0] pixel_rgb,          // pixel_rgb is the data read from the frame buffer
+    input wire [23:0] fill_time,          // time it took to fill the frame buffer in clock cycles
     output reg [143:0] bootload_triangle, // the triangle data read from bootloading
     output wire [31:0] bootload_addr,     // the address in triangle memory to store the triangle
     output wire system_rst_n, TX,         // system_rst_n holds the rest of the system in reset while bootloading
@@ -24,7 +25,7 @@ module bootloader(
     wire [7:0] rx_byte, tx_byte, fifo_wdata;
     wire rx_rdy, q_full, q_empty, tx_done;
     reg rx_rst_n, q_empty_ff;
-    logic count_valid, inc_count, trmt, q_write, q_read, shift_rgb;
+    logic count_valid, inc_count, trmt, q_write, q_read, shift_rgb, read_time, shift_time;
 
     // set the rest of the system in reset during bootload or simply pass rst_n through
     assign system_rst_n = rst_n & ~bootload_en;
@@ -48,11 +49,11 @@ module bootloader(
 // frame buffer TXer //
 //////////////////////
 
-    // instantiate a 3-entry FIFO for sending out frame buffer data
+    // instantiate a 6-entry FIFO for sending out frame buffer and timing data
     FIFO #(
-        .LENGTH(3),    // 3 entries *
+        .LENGTH(6),    // 6 entries *
         .DATA_WIDTH(8) // 8 bits each
-    ) tx_q (           // is enough for 24 bit color
+    ) tx_q (           // is enough for 24 bit color and 24 bit time value
         .clk(clk),
         .rst_n(system_rst_n),
         .read(q_read),
@@ -68,13 +69,13 @@ module bootloader(
     // flop the q_empty signal so that when we write to an empty queue,
     // we wait until the next cycle to consume the data to send over UART
     // which solves the problem of reading and writing to the queue in the same cycle
-    always_ff @(posedge clk, negedge system_rst_n)
-        if (~system_rst_n)
-            q_empty_ff <= 1'b1; // preset the queue as empty
-        else
-            q_empty_ff <= q_empty;
+    // always_ff @(posedge clk, negedge system_rst_n)
+    //     if (~system_rst_n)
+    //         q_empty_ff <= 1'b1; // preset the queue as empty
+    //     else
+    //         q_empty_ff <= q_empty;
 
-    // load the pixel's r, g, or b value into the fifo
+    // load the pixel's r, g, or b value into a register
     reg [23:0] pixel_rgb_ff;
     always_ff @(posedge clk, negedge system_rst_n)
         if (~system_rst_n)
@@ -83,7 +84,6 @@ module bootloader(
             pixel_rgb_ff <= pixel_rgb_ff << 8;
         else if (read_fb)
             pixel_rgb_ff <= pixel_rgb;
-    assign fifo_wdata = pixel_rgb_ff[23:16];
 
     // increment frame buffer address on read
     always_ff @(posedge clk, negedge system_rst_n)
@@ -94,26 +94,45 @@ module bootloader(
     wire done_reading;
     assign done_reading = fb_addr == 2073599; // reached the end of the 1920x1080 buffer
 
-    // 2 bit counter for the state machine, counts to 3, one for each RGB value
+    // load the time it took to fill the frame buffer into a register
+    reg read_time_ff;
+    always_ff @(posedge clk)
+        read_time_ff <= read_time;
+    reg [23:0] time_ff;
+    always_ff @(posedge clk, negedge system_rst_n)
+        if (~system_rst_n)
+            time_ff <= '0;
+        else if (shift_time)
+            time_ff <= time_ff << 8;
+        else if (read_time & ~read_time_ff) // posedge of read_time
+            time_ff <= fill_time;
+
+    // choose which value to load into the fifo
+    assign fifo_wdata = read_time ? time_ff[23:16] : pixel_rgb_ff[23:16];
+
+    // 2 bit counter for the state machine, counts to 3, one for each RGB value, or each byte in the timing data
     reg [1:0] tx_count;
-    wire count_equals_three;
+    wire tx_count_3;
     always_ff @(posedge clk, negedge system_rst_n)
         if (~system_rst_n)
             tx_count <= 2'b00;
         else if (tx_done | q_write)
             tx_count <= tx_count + 1;
-        else if (count_equals_three)
+        else if (tx_count_3)
             tx_count <= 2'b00;
-    assign count_equals_three = &tx_count;
+    assign tx_count_3 = &tx_count;
 
     /*  TX State machine I/O
             Inputs:
                 done_drawing
                 transmit_en
-                count_equals_three
+                tx_count_3
+                tx_count
                 fb_done
             Outputs:
                 trmt
+                read_time
+                shift_time
                 read_fb
                 shift_rgb
                 q_write
@@ -121,7 +140,7 @@ module bootloader(
     */
 
     // TX state flop
-    typedef enum logic [1:0] {IDLE_TX, READ_RGB, TRANSMIT} tx_state_t;
+    typedef enum logic [1:0] {IDLE_TX, READ_TIMING, READ_RGB, TRANSMIT} tx_state_t;
     tx_state_t tx_state, tx_next_state;
     always_ff @(posedge clk, negedge system_rst_n)
         if (~system_rst_n)
@@ -134,6 +153,8 @@ module bootloader(
         // SM defaults, avoid latches
         tx_next_state = tx_state;
         trmt = 1'b0;
+        read_time = 1'b0;
+        shift_time = 1'b0;
         read_fb = 1'b0;
         shift_rgb = 1'b0;
         q_write = 1'b0;
@@ -142,12 +163,23 @@ module bootloader(
         case (tx_state)
             default: // IDLE_TX
                 if (done_drawing & transmit_en) begin
-                    read_fb = 1'b1;
-                    tx_next_state = READ_RGB;
+                    read_time = 1'b1;
+                    tx_next_state = READ_TIMING;
+                end
+
+            READ_TIMING:
+                if (~tx_count_3) begin
+                    q_write = 1'b1;
+                    read_time = 1'b1;
+                    shift_time = 1'b1;
+                end else begin
+                    trmt = 1'b1;
+                    q_read = 1'b1;
+                    tx_next_state = TRANSMIT;
                 end
 
             READ_RGB:
-                if (~count_equals_three) begin
+                if (~tx_count_3) begin
                     q_write = 1'b1;
                     shift_rgb = 1'b1;
                 end else begin
@@ -157,9 +189,9 @@ module bootloader(
                 end
 
             TRANSMIT:
-                if (~count_equals_three) begin
-                    trmt = tx_done;
-                    q_read = tx_done;
+                if (~tx_count_3) begin
+                    trmt = tx_done & ~tx_count[1];
+                    q_read = tx_done & ~tx_count[1];
                 end else if (~done_reading) begin
                     read_fb = 1'b1;
                     tx_next_state = READ_RGB;
